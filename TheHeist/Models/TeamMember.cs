c361d5aa8f3c5c1ede73892e861028dfc99ad211
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-01-19 19:36:40 -0600

    finished phase 2

--- TheHeist/Models/TeamMember.cs
+++ TheHeist/Models/TeamMember.cs
@@ -15,6 +15,18 @@ namespace TheHeist.Models
             CourageFactor = courage;
         }
 
+        public TeamMember(string member, string name, int skill, decimal courage)
+        {
+            Name = name;
+            SkillLevel = skill;
+            CourageFactor = courage;
+        }
+
+        public TeamMember()
+        {
+
+        }
+
         //A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a decimal between 0.0 and 2.0.
         public string Name { get; set; }
         public int SkillLevel { get; set; }
